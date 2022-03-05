7563a980004bfb25ab421ed618a3364d2b2dd119
Breygas Andara <breiighost@gmail.com>
2022-03-06 00:22:05 +0700

    add CRUD

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,2 +1,33 @@
-package com.example.demo.student;public class StudentConfig {
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+//@Configuration
+public class StudentConfig {
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+//        return args -> {
+//            Student breygas = new Student(
+//                    "Breygas",
+//                    "andarabrey@gmail.com",
+//                    LocalDate.of(1994, Month.JUNE, 30)
+//            );
+//            Student andara = new Student(
+//                    "Andara",
+//                    "andarabrey@gmail.com",
+//                    LocalDate.of(1999, Month.JUNE, 30)
+//            );
+//
+//            studentRepository.saveAll(
+//                    List.of(breygas, andara)
+//            );
+//        };
+//    }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,24 +1,54 @@
 package com.example.demo.student;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Breygas",
-                        "andarabrey@gmail.com",
-                        LocalDate.of(2000, Month.FEBRUARY,5),
-                        21
-                )
-        );
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists) {
+            throw new IllegalStateException("student with id " + studentId + " doest not exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("student with id " + studentId + " doest not exists"));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)){
+            student.setEmail(email);
+        }
     }
 }
